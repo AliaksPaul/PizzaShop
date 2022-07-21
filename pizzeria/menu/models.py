7a39e98d8caa72bde74a183bd94c20f427e73f26
Anton Pukhart <anton.pukhart@gmail.com>
2022-07-21 21:39:37 +0300

    class Salad(models.Model):

--- pizzeria/menu/models.py
+++ pizzeria/menu/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    medium_price = models.DecimalField(max_digits=10, decimal_places=2) 
+    large_price = models.DecimalField(max_digits=10, decimal_places=2) 
+
+    def __str__(self):
+        return self.name
+
+
+class Salad(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+
+    def __str__(self):
+        return self.name
+
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+
+    def __str__(self):
+        return self.name
