890c8ec9569063e21cffc9d83af0b5daf2989a89
Antoine <anton.pukhart@gmail.com>
2022-08-01 17:25:16 +0300

    feature: add user registration

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django import forms
+
+
+class UserRegistrationForm(forms.ModelForm):
+    username = forms.CharField(label="Username", widget=forms.TextInput)
+    email = forms.CharField(label="Email", widget=forms.EmailInput)
+    first_name = forms.CharField(label="Enter your first name", widget=forms.TextInput)
+    last_name = forms.CharField(label="Enter your last name", widget=forms.TextInput)
+    password = forms.CharField(label="Password:", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repeat your Password:", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data['password'] != data['password2']:
+            raise forms.ValidationError("Passwords didn't match!")
+        return data['password2']
